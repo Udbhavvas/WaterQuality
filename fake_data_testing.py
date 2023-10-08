383fd31aae1be52276cee655bbdd312e37d88502
Udbhavvas <udbhavv@umich.edu>
2023-10-08 15:13:24 -0400

    visualization

--- fake_data_testing.py
+++ fake_data_testing.py
@@ -3,24 +3,43 @@ import SVM
 import numpy as np
 import pandas as pd
 from sklearn import datasets
-import matplotlib as plt
+from matplotlib import pyplot as plt
 
 from sklearn.model_selection import train_test_split
 
 ##################
 n_pts = 1000
 np.random.seed(0)
-Xa = np.array([np.random.normal(13, 2, n_pts),
-              np.random.normal(12, 2, n_pts)]).T
-Xb = np.array([np.random.normal(8, 2, n_pts),
-              np.random.normal(6, 2, n_pts)]).T
- 
+Xa = np.array([np.random.normal(15, 1.75, n_pts),
+              np.random.normal(15, 1.75, n_pts)]).T
+Xb = np.array([np.random.normal(8, 1.75, n_pts),
+              np.random.normal(6, 1.75, n_pts)]).T
+
 X = np.vstack((Xa, Xb))
-Y = np.matrix(np.append(np.zeros(n_pts), np.ones(n_pts))).T
+negative_ones = np.full((1, n_pts), -1)
+#print(y)
+positive_ones = np.full((1, n_pts), 1)
+Y = np.matrix(np.append(negative_ones, positive_ones)).T
  
 
-plt.scatter(X[:n_pts,0], X[:n_pts,1])
-plt.scatter(X[n_pts:,0], X[n_pts:,1])
+#Class 1 X and Y values
+
+Class1 = [X[:n_pts,0], X[:n_pts,1]]
+plt.scatter(Class1[0], Class1[1])
+#plt.scatter(X[:n_pts,0], X[:n_pts,1])
+
+#CLass 2 X and Y values
+
+Class2 = [X[n_pts:,0], X[n_pts:,1]]
+plt.scatter(Class2[0], Class2[1])
+# plt.scatter(X[n_pts:,0], X[n_pts:,1])
+plt.show()
+
+#Class1[0] Class2[0]
+svm = SVM.SVM()
+svm.train(X,Y)
+y_pred = svm.predict(X,Y)
+print(svm.accuracy(Y, y_pred))
 ########################
 
 
